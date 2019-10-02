75b87ec6fbe52c0c97e2f0cc8219ef14815fee74
Jason Kao <jason.kao@columbia.edu>
2019-10-02 10:41:23 -0400

    new herd data

--- r/graphics.R
+++ r/graphics.R
@@ -1,16 +1,26 @@
 library(tidyverse)
 
+# Clean the data, then reorder sources in ascending
+# order (more aesthetic)
 data <- readxl::read_excel("../HERD_Export_cleaned.xlsx") %>% 
   filter(Source != "Total for selected values") %>% 
-  mutate(Amount = as.numeric(Amount))
+  mutate(Amount = as.numeric(Expenditures)) %>% 
+  mutate(
+    Source = factor(
+      Source,
+      levels = c(
+        "All other sources",
+        "State and local government",
+        "Nonprofit organizations",
+        "Business",
+        "Institutional funds",
+        "U.S. federal government"
+      )
+    )
+  )
 
 # small multiples, should reorder them in ascending order
 data %>%
-  ggplot(aes(Year, Amount)) +
+  ggplot(aes(Year, Expenditures)) +
   geom_line() +
   facet_grid(~ Source)
-
-# spaghetti plot
-data %>%
-  ggplot(aes(Year, Amount)) +
-  geom_line(aes(color = Source))
